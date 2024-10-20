554ba075f4ebf6281686ff09bf8b2ec573c7630e
Sidhartha-m <148371448+SidharthaSai9@users.noreply.github.com>
2024-10-20 12:02:55 -0500

    ffnn.py

--- ffnn.py
+++ ffnn.py
@@ -7,6 +7,7 @@ import math
 import random
 import os
 import time
+import string
 from tqdm import tqdm
 import json
 from argparse import ArgumentParser
@@ -80,22 +81,25 @@ def convert_to_vector_representation(data, word2index):
 
 
 
-def load_data(train_data, val_data):
+def load_data(train_data, val_data, test_data):
     with open(train_data) as training_f:
         training = json.load(training_f)
-        print(f"Number of training examples: {len(training)}") #Added by me
     with open(val_data) as valid_f:
         validation = json.load(valid_f)
-        print(f"Number of validation examples: {len(validation)}") #Added by me
+    with open(test_data) as test_f:
+        test = json.load(test_f)
 
     tra = []
     val = []
+    tes = []
     for elt in training:
         tra.append((elt["text"].split(),int(elt["stars"]-1)))
     for elt in validation:
         val.append((elt["text"].split(),int(elt["stars"]-1)))
+    for elt in test:
+        tes.append((elt["text"].split(),int(elt["stars"]-1)))
 
-    return tra, val
+    return tra, val, tes
 
 
 if __name__ == "__main__":
@@ -104,7 +108,7 @@ if __name__ == "__main__":
     parser.add_argument("-e", "--epochs", type=int, required = True, help = "num of epochs to train")
     parser.add_argument("--train_data", required = True, help = "path to training data")
     parser.add_argument("--val_data", required = True, help = "path to validation data")
-    parser.add_argument("--test_data", default = "to fill", help = "path to test data")
+    parser.add_argument("--test_data", required = True, help = "path to test data")
     parser.add_argument('--do_train', action='store_true')
     args = parser.parse_args()
 
@@ -114,13 +118,14 @@ if __name__ == "__main__":
 
     # load data
     print("========== Loading data ==========")
-    train_data, valid_data = load_data(args.train_data, args.val_data) # X_data is a list of pairs (document, y); y in {0,1,2,3,4}
+    train_data, valid_data, test_data = load_data(args.train_data, args.val_data, args.test_data) # X_data is a list of pairs (document, y); y in {0,1,2,3,4}
     vocab = make_vocab(train_data)
     vocab, word2index, index2word = make_indices(vocab)
 
     print("========== Vectorizing data ==========")
     train_data = convert_to_vector_representation(train_data, word2index)
     valid_data = convert_to_vector_representation(valid_data, word2index)
+    test_data = convert_to_vector_representation(test_data, word2index)
     
 
     model = FFNN(input_dim = len(vocab), h = args.hidden_dim)
@@ -186,4 +191,26 @@ if __name__ == "__main__":
         print("Validation time for this epoch: {}".format(time.time() - start_time))
 
     # write out to results/test.out
+    if args.test_data:
+        print("========== Testing started ==========")
+        
+        model.eval()  # Set model to evaluation mode
+        correct = 0
+        total = 0
+        minibatch_size = 16 
+        N = len(test_data)
+
+        # Iterate through test data
+        for minibatch_index in tqdm(range(N // minibatch_size)):
+            for example_index in range(minibatch_size):
+                input_vector, gold_label = test_data[minibatch_index * minibatch_size + example_index]
+                with torch.no_grad():  # No gradient calculation in test
+                    predicted_vector = model(input_vector)
+                    predicted_label = torch.argmax(predicted_vector)
+                
+                correct += int(predicted_label == gold_label)
+                total += 1
+
+        print("Test accuracy: {:.2f}%".format(100 * correct / total))
     
+
